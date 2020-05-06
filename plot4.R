9136bb47cd79987719f0fbd3eb7e8d5972a0e4d4
Bridget McGuire <bmcguir8@jhmi.edu>
2020-05-06 12:31:50 -0400

    edited readability

--- plot4.R
+++ plot4.R
@@ -11,15 +11,23 @@ plot4 <- {
   data <- mutate(data, Date.Time = dmy_hms(data$Date.Time))
   png(filename = "plot4.png")
   par(mfcol = c(2, 2))
+  
+    ##plot 1
   plot(data$Date.Time, data$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)",
        type = "l")
+  
+    ##plot 2
   plot(data$Date.Time, data$Sub_metering_1, type = "l", xlab = "", 
        ylab = "Energy sub metering")
   points(data$Date.Time, data$Sub_metering_2, type = "l", col = "red")
   points(data$Date.Time, data$Sub_metering_3, type = "l", col = "blue")
   legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-         lty = 1, bty = "n")
+         lty = 1, bty = "n", cex = 0.7)
+  
+    ##plot 3
   plot(data$Date.Time, data$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
+  
+    ##plot 4
   plot(data$Date.Time, data$Global_reactive_power, type = "l", xlab = "datetime",
        ylab = "Global_reactive_power")
   dev.off()
