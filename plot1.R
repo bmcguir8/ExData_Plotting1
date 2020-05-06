f6ac69bc0b3a12a459845846679a86b6d3c04dcb
Bridget McGuire <bmcguir8@jhmi.edu>
2020-05-06 11:22:49 -0400

    Removed unnecessary code (the date column doesn't need to be reformatted for this code)

--- plot1.R
+++ plot1.R
@@ -6,7 +6,6 @@ plot1 <- {
   data <- read.table("household_power_consumption.txt", header = TRUE, 
        na.strings = "?", sep = ";", skip = 66636, nrows = 2879) ##reads in select rows
   names(data) <- header
-  data <- mutate(data, Date = dmy(data$Date))
   png(filename = "plot1.png")
   hist(data$Global_active_power, col = "red", breaks = 12, main = "Global Active Power",
        xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
